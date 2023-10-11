92b3f8cdaa605231b4f145476e628da049b8bf17
Klevert Opee <81738300+klevert-ope@users.noreply.github.com>
2023-10-11 17:46:27 +0300

    Scroll smoother code performance improvements

--- hooks/useScrollSmoother.js
+++ hooks/useScrollSmoother.js
@@ -1,25 +1,20 @@
-import { useEffect } from "react";
+import { useLayoutEffect } from "react";
 import gsap from "gsap";
 import ScrollSmoother from "gsap/dist/ScrollSmoother.js";
 import ScrollTrigger from "gsap/dist/ScrollTrigger.js";
 
-// Register GSAP plugins
 gsap.registerPlugin(ScrollSmoother, ScrollTrigger);
 
 export function useScrollSmoother() {
-  useEffect(() => {
+  useLayoutEffect(() => {
     const initializeScrollSmoother = async () => {
-      // Configure GSAP with authentication token and other options
       gsap.config({
         auth: process.env.gsap_authToken,
       });
 
-      // Create a GSAP context
       const gsapContext = gsap.context(() => {
-        // Normalize scroll behavior
         ScrollTrigger.normalizeScroll(true);
 
-        // Create the ScrollSmoother instance
         const scrollSmoother = ScrollSmoother.create({
           wrapper: "#smooth-wrapper",
           content: "#smooth-content",
@@ -28,17 +23,14 @@ export function useScrollSmoother() {
           smoothTouch: 0.1,
         });
 
-        // Return a cleanup function
         return () => {
           scrollSmoother.kill();
         };
       });
 
-      // Revert the GSAP context on unmount
       return () => gsapContext.revert();
     };
 
-    // Initialize ScrollSmoother when the component mounts
     initializeScrollSmoother();
-  }, []); // Run this effect only once on component mount
+  }, []);
 }
