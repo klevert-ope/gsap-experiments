715f4e03dab5906fe1958124948133116a3b41e8
Klevert Opee <81738300+klevert-ope@users.noreply.github.com>
2023-10-31 22:28:19 +0300

    resolved a redundant variable

--- hooks/video hooks/useVideoPlayer.jsx
+++ hooks/video hooks/useVideoPlayer.jsx
@@ -1,34 +1,33 @@
-import { useEffect, useRef } from "react";
+import {useEffect, useRef} from "react";
 import Hls from "hls.js";
 
 export function useVideoPlayer(videoSource) {
-  const videoRef = useRef(null);
-  const hlsRef = useRef(null);
+    const videoRef = useRef(null);
+    const hlsRef = useRef(null);
 
-  useEffect(() => {
-    if (videoSource && Hls.isSupported()) {
-      const video = videoRef.current;
+    useEffect(() => {
+        if (videoSource && Hls.isSupported()) {
+            const video = videoRef.current;
 
-      if (!video) {
-        const newVideo = document.createElement("video");
-        videoRef.current = newVideo;
-      }
+            if (!video) {
+                videoRef.current = document.createElement("video");
+            }
 
-      hlsRef.current = new Hls();
-      hlsRef.current.loadSource(videoSource);
-      hlsRef.current.attachMedia(video);
-      hlsRef.current.on(Hls.Events.MANIFEST_PARSED, () => {
-        video.play();
-      });
+            hlsRef.current = new Hls();
+            hlsRef.current.loadSource(videoSource);
+            hlsRef.current.attachMedia(video);
+            hlsRef.current.on(Hls.Events.MANIFEST_PARSED, () => {
+                video.play();
+            });
 
-      return () => {
-        if (hlsRef.current) {
-          hlsRef.current.off(Hls.Events.MANIFEST_PARSED);
-          hlsRef.current.destroy();
+            return () => {
+                if (hlsRef.current) {
+                    hlsRef.current.off(Hls.Events.MANIFEST_PARSED);
+                    hlsRef.current.destroy();
+                }
+            };
         }
-      };
-    }
-  }, [videoSource]);
+    }, [videoSource]);
 
-  return { videoRef };
+    return {videoRef};
 }
