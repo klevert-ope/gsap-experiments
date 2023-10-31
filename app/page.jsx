c0b630ce461704bd1bbf96d8682b58903ec06e01
Klevert Opee <81738300+klevert-ope@users.noreply.github.com>
2023-10-31 19:46:53 +0300

    file structure

--- app/page.jsx
+++ app/page.jsx
@@ -1,29 +1,27 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "react-query";
-import { useScrollSmoother } from "@/hooks/useScrollSmoother";
+import { useScrollSmoother } from "@/hooks/ui hooks/useScrollSmoother";
 import ScrollThumb from "@/components/ui/ScrollThumb";
 import S1 from "@/components/sections/S1";
 import S2 from "@/components/sections/S2";
-import VideoMobile from "@/components/VideoMobile";
-import VideoLaptop from "@/components/VideoLaptop";
 
 const queryClient = new QueryClient();
 export default function Home() {
   useScrollSmoother();
   return (
     <>
-      <div id="smooth-wrapper">
-        <div id="smooth-content">
-          <QueryClientProvider client={queryClient}>
-            <VideoLaptop />
-            <VideoMobile />
-          </QueryClientProvider>
-          <S1 />
-          <S2 />
+      <QueryClientProvider client={queryClient}>
+        <div id="smooth-wrapper">
+          <div id="smooth-content">
+            <main>
+              <S1 />
+              <S2 />
+            </main>
+          </div>
         </div>
-      </div>
-      <ScrollThumb />
+        <ScrollThumb />
+      </QueryClientProvider>
     </>
   );
 }
--- components/sections/S2.jsx
+++ components/sections/S2.jsx
@@ -2,7 +2,7 @@ const S1 = () => {
   return (
     <>
       <section>
-        <p data-lag="clamp(0.2)" className="layout">
+        <p data-lag="0.2" className="layout">
           In the world of transportation, flying cars had transitioned from
           science fiction to reality. Cities had sprawling networks of automated
           aerial taxis, eliminating traffic congestion and significantly
@@ -10,7 +10,7 @@ const S1 = () => {
           norm, making accidents a rarity. The streets were quieter, and the air
           was cleaner.
         </p>
-        <p data-lag="clamp(0.4)" className="layout">
+        <p data-lag="0.4" className="layout">
           Education had transformed as well. Traditional classrooms were a thing
           of the past. Students now entered virtual reality environments where
           they could explore history, travel to different eras, and interact
