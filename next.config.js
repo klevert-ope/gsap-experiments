c7458c689c01b1c68f760914041375096681f3b3
Klevert Opee <81738300+klevert-ope@users.noreply.github.com>
2023-10-31 19:43:21 +0300

    change of package manager and env in next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    SUPABASE_URL: process.env.SUPABASE_URL,
+    SUPABASE_KEY: process.env.SUPABASE_KEY,
+    GSAP_TOKEN: process.env.GSAP_TOKEN,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
